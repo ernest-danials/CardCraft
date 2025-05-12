c80c98a2601968a419a49e86159cc751671e5565
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2025-05-12 08:32:23 -0700

    Initial Commit

---
+++ CardCraft/CardCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardCraftApp.swift
+//  CardCraft
+//
+//  Created by Myung Joon Kang on 2025-05-12.
+//
+
+import SwiftUI
+
+@main
+struct CardCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
